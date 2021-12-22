29130533a6c7629e49eceac77bcedb34c2d1dd8f
Алибек <70684705+yretrut@users.noreply.github.com>
2021-12-22 14:16:38 +0300

    Update index.js

--- lab5/index.js
+++ lab5/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
-
 const app = express();
 const port = 3000;
+
 app.use(express.json());
 app.use("/api", require("./student"));
+app.get("/", (req, res) => res.send("ok"));
+app.all("*", (req, res) =>res.send("not found"));
 
-app.listen(port, () => {
-  console.log(`http://localhost:${port}`);
-});
+app.listen(port, () => { console.log(`http://localhost:${port}`); });
