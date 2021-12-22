b152e9b4e7277b41aee7ae037c90cf440a9c9953
Алибек <70684705+yretrut@users.noreply.github.com>
2021-12-22 14:22:56 +0300

    Update index.js

--- lab2/src/index.js
+++ lab2/src/index.js
@@ -1,14 +1,14 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
-var array = [];
+var array = []; //создание массива в котором будет хранится отправленная информация
 
 app.use(bodyParser.text());
 app.get("/", (req, res) => res.send("Жду запрос =)"));
-app.get("/list", function (req, res) {
+app.get("/list", function (req, res) { // вывод данных
   res.send(array.toString());
 });
-app.post("/create", function (req, res) {
+app.post("/create", function (req, res) { // отправка данных
   array.push(req.body);
   console.log(req.body);
   res.send("Good");
